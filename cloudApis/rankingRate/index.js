94652c52854963108d88a2d2d9e535039be4c749
wuhuanda <583783099@qq.com>
2020-07-15 21:48:57 +0800

    feat: 完善排行榜功能

--- cloudApis/rankingRate/index.js
+++ cloudApis/rankingRate/index.js
@@ -6,6 +6,7 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const db = cloud.database()
+  const cf = cloud.callFunction
   let result = {}
 
   await db
@@ -17,13 +18,19 @@ exports.main = async (event, context) => {
       if (data.length === 0) {
         result = { code: 500, message: '未有用户参与排名' }
       } else {
-        const ranking = data.reduce((acc, cur) => {
-          const { funds } = cur
+        const ranking = data.reduce(async (acc, cur) => {
+          const { nickName, funds } = cur
           if (funds) {
             funds.sort((a, b) => b.rate - a.rate)
-            const info = { }
-            return acc.concat([funds[0]])
+            const { code, rate } = funds[0]
+            const info = { nickName: nickName, code, rate }
+            await cf({ name: 'getFundDetail', data: { code } }).then((fundDetail) => {
+              const { name } = fundDetail.result.data
+              info.fundName = name
+            })
+            acc.push(info)
           }
+          return acc
         }, [])
         result = { code: 200, message: 'success', data: ranking }
       }
